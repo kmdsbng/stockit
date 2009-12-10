128e1f35ccb5ec2c3e12450e8b5879e0070f79f7
unknown <kameda@.(none)>
2009-12-10 17:02:44 +0900

    fix: icon localtion.

--- stockit.js
+++ stockit.js
@@ -75,7 +75,7 @@ function removeStorage(url) {
     stockList.urllist = result;
 }
 
-const SLIDEBAR_ICON = "http://kmdsbng.railsplayground.net/stockit_icon.png";
+const SLIDEBAR_ICON = "http://github.com/kmdsbng/stockit/raw/master/img/stockit_icon.png";
 const DEFAULT_FAVICON = "http://tb4.fr/labs/jetpack/thumbtabs/favicon.png";
 const CLOSE_TAB_ICON = "http://tb4.fr/labs/jetpack/thumbtabs/close.png";
 const NEW_TAB_ICON = "http://tb4.fr/labs/jetpack/thumbtabs/new_tab.png";
